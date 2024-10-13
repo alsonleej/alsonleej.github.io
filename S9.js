1823ee89deb9b7909a06b9824b0861b593dd63dd
Source Academy User <No public email provided>
2024-10-14 02:22:18 +0800

    Changes made from Source Academy

---
+++ S9.js
@@ -0,0 +1,20 @@
+function d_filter(pred, xs) {
+    if (is_null(xs)){
+        return null;
+    }
+    else{
+
+        if (pred(head(xs))){
+            set_tail(xs, d_filter(pred, tail(xs)));
+        }
+        else {
+            xs = tail(xs);
+            xs = d_filter(pred, xs);
+        }
+        return xs;
+    }
+}
+///Example call:
+const L = list(1, 2, 3, 4, 5, 6, 7, 8, 9, 11);
+d_filter(x => x % 2 === 0, L); // returns [2, [4, [6, [8, null]]]]
+L; // What is L now?
